7b988243752d9aea96cf45a429aa22b05d785d71
ninedraft <clamores.pro@gmail.com>
2019-02-14 15:24:24 +0300

    hardcode hard heart

---
+++ heart.go
@@ -0,0 +1,3 @@
+package main
+
+const heart = "  ,ad8PPPP88b,     ,d88PPPP8ba,   \n d8P\"      \"Y8b, ,d8P\"      \"Y8b  \ndP'           \"8a8\"           `Yd \n8(              \"              )8 \nI8                             8I \n Yb,                         ,dP  \n  \"8a,                     ,a8\"   \n    \"8a,                 ,a8\"     \n      \"Yba             adP\"       \n        `Y8a         a8P'         \n          `88,     ,88'           \n            \"8b   d8\"             \n             \"8b d8\"              \n              `888'               \n                \"                  "
